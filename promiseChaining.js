2f4d458857be07a18ed1e98d732163c7657757bf
Vivek Kumar <clocktower77@protonmail.com>
2023-03-06 17:15:36 +0530

    promise chaining

---
+++ promiseChaining.js
@@ -0,0 +1,16 @@
+new Promise(function(resolve, reject) {
+    setTimeout(() => resolve(1), 1000);
+    }).then(function(result) {
+    console.log(result); // 1
+    return result * 2;
+    }).then(function(result) {
+    console.log(result); // 2
+    return result * 3;
+    }).then(function(result) {
+    console.log(result); // 6
+    return result * 4;
+    });
+
+    // Process of executing a sequence of asynchronous tasks one after another using promises is
+// known as Promise chaining. 
+
